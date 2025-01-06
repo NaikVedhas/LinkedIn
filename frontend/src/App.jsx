963b56d6fcb0f3d37f07b1cdcd1c6f8a9a33afd7
NaikVedhas <vedhasnaik121@gmail.com>
2025-01-06 11:47:13 +0530

    Frontend started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
-
+import Layout from "./Layout"
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
+import Home from "./pages/Home"
+import NotFound  from "./pages/NotFound";
+import { createBrowserRouter,Route,createRoutesFromElements,RouterProvider } from "react-router"
 
 function App() {
 
-  return (
-   <>
-   <h1>dhvwq</h1>
-   </>
-  )
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout/>}>
+        <Route path="" element={<Home/>}/>
+        <Route path="login" element={<Login/>}/>
+        <Route path="signup" element={<Signup/>}/>
+        <Route path="*" element={<NotFound/>}/> 
+      </Route>
+    )
+  );
+
+
+  return (<RouterProvider router={router}/>)
 }
 
 export default App
