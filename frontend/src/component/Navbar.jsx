a23e47a28d095f42b4f0cf7db181e5f542e1ecc0
NaikVedhas <vedhasnaik121@gmail.com>
2025-01-08 20:03:48 +0530

    Updated the Navbar

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,121 +1,143 @@
-import {useMutation, useQuery,useQueryClient} from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
-import {Link} from "react-router-dom"
+import { Link, NavLink } from "react-router-dom";
 import { Bell, Home, LogOut, User, Users } from "lucide-react";
 
-
 const Navbar = () => {
-  
-  const {data:authUser} = useQuery({queryKey:["authUser"]});
-  const queryClient = useQueryClient(); 
-  
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+  const queryClient = useQueryClient();
 
-  //Now we want to get notifications and connections only when we ahve authUser
-  const {data:notifications} = useQuery({
-    queryKey:["notifications"],
+  // Now we want to get notifications and connections only when we have authUser
+  const { data: notifications } = useQuery({
+    queryKey: ["notifications"],
     queryFn: async () => {
       const res = await axiosInstance.get("/notifications/");
       return res;
     },
-    enabled: !!authUser   //this means that this query will run only when we have authUser 
+    enabled: !!authUser, // this means that this query will run only when we have authUser
   });
 
-  const {data:connectionRequests} = useQuery({
-    queryKey:["connectionRequests"],
-    queryFn: async()=> {
+  const { data: connectionRequests } = useQuery({
+    queryKey: ["connectionRequests"],
+    queryFn: async () => {
       const res = await axiosInstance.get("/connections/requests");
       return res;
-    }, 
-    enabled:!!authUser
-  });
-
-  
-  const {mutate:logout} = useMutation({
-    mutationFn: async () => axiosInstance.post('/auth/logout'),
-    onSuccess:()=>{
-      toast.success("Logged Out");
-      queryClient.invalidateQueries({queryKey:["authUser"]}); // so it will reftech this and authUser is null we will be redirected to login pages without refreshing
-
     },
-    onError:(error)=>{
-      toast.error("Something went wrong");
-    }
+    enabled: !!authUser,
   });
-  
-  const unreadNotificationCount = notifications?.data.filter((notif) => !notif.read).length;
-  const unreadConnectionRequestsCount = connectionRequests?.data?.length;;
-  
+
+  const { mutate: logout } = useMutation({
+    mutationFn: async () => axiosInstance.post("/auth/logout"),
+    onSuccess: () => {
+      toast.success("Logged Out");
+      queryClient.invalidateQueries({ queryKey: ["authUser"] }); // so it will refresh this and authUser is null we will be redirected to login pages without refreshing
+    },
+    onError: (error) => {
+      toast.error("Something went wrong");
+    },
+  });
+
+  const unreadNotificationCount = notifications?.data.filter(
+    (notif) => !notif.read
+  ).length;
+  const unreadConnectionRequestsCount = connectionRequests?.data?.length;
 
   return (
-        <nav className='bg-secondary shadow-md sticky top-0 z-10 mb-5'>
-          <div className='max-w-7xl mx-auto px-4'>
-           <div className='flex justify-between items-center py-3'>
-              <div className='flex items-center space-x-4'>
-                <Link to='/'>
-                  <img className='h-8 rounded' src='/small-logo.png' alt='LinkedIn' />
+    <nav className="bg-secondary shadow-md sticky top-0 z-10 mb-5">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between items-center py-3">
+          <div className="flex items-center space-x-4">
+            <Link to="/">
+              <img className="h-8 rounded" src="/small-logo.png" alt="LinkedIn" />
+            </Link>
+          </div>
+          <div className="flex items-center gap-2 md:gap-6">
+            {authUser ? (
+              <>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-primary flex flex-col items-center font-bold" // active icon becomes bold and colored
+                      : "text-neutral flex flex-col items-center hover:text-primary"
+                  }
+                >
+                  <Home size={20} />
+                  <span className="text-xs hidden md:block">Home</span>
+                </NavLink>
+                <NavLink
+                  to="/network"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-primary flex flex-col items-center font-bold" // active icon becomes bold and colored
+                      : "text-neutral flex flex-col items-center hover:text-primary"
+                  }
+                >
+                  <Users size={20} />
+                  <span className="text-xs hidden md:block">My Network</span>
+                  {unreadConnectionRequestsCount > 0 && (
+                    <span
+                      className="absolute -top-1 -right-1 md:right-4 bg-blue-500 text-white text-xs 
+                      rounded-full size-3 md:size-4 flex items-center justify-center"
+                    >
+                      {unreadConnectionRequestsCount}
+                    </span>
+                  )}
+                </NavLink>
+                <NavLink
+                  to="/notifications"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-primary flex flex-col items-center font-bold" // active icon becomes bold and colored
+                      : "text-neutral flex flex-col items-center hover:text-primary"
+                  }
+                >
+                  <Bell size={20} />
+                  <span className="text-xs hidden md:block">Notifications</span>
+                  {unreadNotificationCount > 0 && (
+                    <span
+                      className="absolute -top-1 -right-1 md:right-4 bg-blue-500 text-white text-xs 
+                      rounded-full size-3 md:size-4 flex items-center justify-center"
+                    >
+                      {unreadNotificationCount}
+                    </span>
+                  )}
+                </NavLink>
+                <NavLink
+                  to={`/profile/${authUser.username}`}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-primary flex flex-col items-center font-bold" // active icon becomes bold and colored
+                      : "text-neutral flex flex-col items-center hover:text-primary"
+                  }
+                >
+                  <User size={20} />
+                  <span className="text-xs hidden md:block">Me</span>
+                </NavLink>
+                <button
+                  className="flex items-center space-x-1 text-sm text-gray-600 hover:text-gray-800"
+                  onClick={() => logout()}
+                >
+                  <LogOut size={20} />
+                  <span className="hidden md:inline">Logout</span>
+                </button>
+              </>
+            ) : (
+              <>
+                <Link to="/login" className="btn btn-ghost">
+                  Sign In
                 </Link>
-              </div>
-            <div className='flex items-center gap-2 md:gap-6'>
-              {authUser ? (
-                <>
-                  <Link to={"/"} className='text-neutral flex flex-col items-center'>
-                    <Home size={20} />
-                    <span className='text-xs hidden md:block'>Home</span>
-                  </Link>
-                  <Link to='/network' className='text-neutral flex flex-col items-center relative'>
-                    <Users size={20} />
-                    <span className='text-xs hidden md:block'>My Network</span>
-                    {unreadConnectionRequestsCount > 0 && (
-                      <span
-                        className='absolute -top-1 -right-1 md:right-4 bg-blue-500 text-white text-xs 
-                      rounded-full size-3 md:size-4 flex items-center justify-center'
-                      >
-                        {unreadConnectionRequestsCount}
-                      </span>
-                    )}
-                  </Link>
-                  <Link to='/notifications' className='text-neutral flex flex-col items-center relative'>
-                    <Bell size={20} />
-                    <span className='text-xs hidden md:block'>Notifications</span>
-                    {unreadNotificationCount > 0 && (
-                      <span
-                        className='absolute -top-1 -right-1 md:right-4 bg-blue-500 text-white text-xs 
-                      rounded-full size-3 md:size-4 flex items-center justify-center'
-                      >
-                        {unreadNotificationCount}
-                      </span>
-                    )}
-                  </Link>
-                  <Link
-                    to={`/profile/${authUser.username}`}
-                    className='text-neutral flex flex-col items-center'
-                  >
-                    <User size={20} />
-                    <span className='text-xs hidden md:block'>Me</span>
-                  </Link>
-                  <button
-                    className='flex items-center space-x-1 text-sm text-gray-600 hover:text-gray-800'
-                    onClick={() => logout()}
-                  >
-                    <LogOut size={20} />
-                    <span className='hidden md:inline'>Logout</span>
-                  </button>
-                </>
-              ) : (
-                <>
-                  <Link to='/login' className='btn btn-ghost'>
-                    Sign In
-                  </Link>
-                  <Link to='/signup' className='btn btn-primary'>
-                    Join now
-                  </Link>
-                </>
-              )}
-            </div>
+                <Link to="/signup" className="btn btn-primary">
+                  Join now
+                </Link>
+              </>
+            )}
           </div>
         </div>
-      </nav>
-  )
-}
-export default Navbar
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
