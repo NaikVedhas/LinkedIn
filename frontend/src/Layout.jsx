d230cc8543410dcb373e64b862140735ff2e94d4
NaikVedhas <vedhasnaik121@gmail.com>
2025-01-16 12:56:50 +0530

    Added ScrollToTop

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,10 +1,13 @@
 import {Outlet} from "react-router-dom";
 import Navbar from "./component/Navbar";
+import ScrollToTop from "./ScrollToTop";
 
+//Aded scroll to top so that when u go from one page to aother u go at the top of another page
 function Layout () {
 
     return (
         <>
+        <ScrollToTop/>
         <Navbar/>
         <Outlet/>
         </>
