0cb73df75f7aa5bc1b6b97a156f4dc632fb0b75d
NaikVedhas <vedhasnaik121@gmail.com>
2025-02-03 16:50:27 +0530

    Context Created

---
+++ frontend/src/context/MessageUserContext.jsx
@@ -0,0 +1,18 @@
+//This context will store the selected user(the user whose chats will be shown)
+
+import { useContext,createContext, useState } from "react";
+
+export const MessageUserContext = createContext(null);
+
+export const useMessageUserContext = () =>{
+  return useContext(MessageUserContext);
+}
+
+export const MessageUserContextProvider = (props) => {
+   
+    const [selectedUser,setSelectedUser] = useState("");
+
+   return <MessageUserContext.Provider value={{selectedUser,setSelectedUser}}>
+        {props.children}
+    </MessageUserContext.Provider>
+}
