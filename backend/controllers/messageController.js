a1212aef63ebf542c4efdcc6dcbbff90b558967c
NaikVedhas <vedhasnaik121@gmail.com>
2025-02-03 15:16:35 +0530

    Completed sendMessages

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,5 +1,6 @@
 const Message = require('../models/messagesModel');
 const User = require('../models/userModel');
+const cloudinary = require('../lib/cloudinary');
 
 const getSidebarUsers = async (req,res)=>{
 
@@ -50,9 +51,41 @@ const getAllMessages = async (req,res) => {
 
 }
 
+const sendMessages = async (req,res) =>{
+    
+    try {
+        const {id} = req.params;  //we will send message to this person
+        const {text,image} = req.body;
 
+        console.log("receiverId",id);
+        console.log("sender",req.user._id);
+        
+        let imageURL;
+
+        if(image){
+            //Upload to cloundinary 
+            const result = await cloudinary.uploader.upload(image);
+            imageURL=result.secure_url;
+        }
+        
+        const message = await Message.create({senderId:req.user._id,receiverId:id,text:text,image:imageURL},{new:true}); 
+        //if there is image then it will store ulr or else it will store undefined
+        
+        if(!message){
+            return res.status(400).json({message:"Error storing message"});
+        }
+
+        //todo add realtime functionality with socketio here 
+        res.status(200).json(message);
+
+    } catch (error) {
+        console.log("Error in sendMessage",error);
+        res.status(500).json({message:"Server Error"});
+    }
+}
 
 module.exports = {
     getSidebarUsers,
-    getAllMessages
+    getAllMessages,
+    sendMessages
 }
