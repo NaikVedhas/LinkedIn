05a8ff2e6503063ca2b161f00306e2fa7e7a6764
NaikVedhas <vedhasnaik121@gmail.com>
2025-02-03 14:42:59 +0530

    Completed getAllmessages

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -20,6 +20,39 @@ const getSidebarUsers = async (req,res)=>{
     }
 }
 
+const getAllMessages = async (req,res) => {
+    //We are taking the conversation between 2 ppl
+
+    
+    try {
+       const {id} = req.params;   //id of the next person
+        
+       const messages = await Message.find({
+        $or:[
+            {receiverId:id,senderId:req.user._id},//either this and the comma inside the {} means the and opertion
+            {receiverId:req.user._id,senderId:id}          //or this
+        ]}
+       )
+       .populate("senderId","-password")
+       .populate("receiverId","-password")
+       .sort({createdAt:-1});
+
+       if(!messages){
+        return res.status(404).json({message:"No messages found"});
+       }
+
+       res.status(200).json(messages);
+
+   } catch (error) {
+        console.log("Error in getAllMessages",error);
+        res.status(500).json({message:"Server Error"});
+   }
+
+}
+
+
+
 module.exports = {
-    getSidebarUsers
+    getSidebarUsers,
+    getAllMessages
 }
--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,10 +1,10 @@
-const {getSidebarUsers} = require("../controllers/messageController");
+const {getSidebarUsers,getAllMessages} = require("../controllers/messageController");
 const express = require('express');
 const protectRoute = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get("/users",protectRoute,getSidebarUsers);
-
+router.get("/:id",protectRoute,getAllMessages)
 
 module.exports = router
 
